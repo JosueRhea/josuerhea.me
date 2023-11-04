72d33cd9e363d86e66b966f133aacb8e35ae6b6f
Josue A. <josueroyale8171@gmail.com>
2023-11-04 16:10:13 +0000

    Fix: opengraph-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,19 @@ import { GeistSans } from "geist/font";
 export const metadata: Metadata = {
   title: "Josue A. - @josuerhea",
   description: "A software engineer that makes stuff on the web thingy.",
+  metadataBase: new URL("https://josuerhea.me"),
+  openGraph: {
+    title: "Josue A. - @josuerhea",
+    description: "Just a software engineer.",
+    url: "https://josuerhea.me",
+    siteName: "Josue A. - @josuerhea",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    title: 'Josue A.',
+    card: 'summary_large_image',
+  },
 };
 
 export default function RootLayout({
