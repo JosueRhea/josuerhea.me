de8816e4f7d85742de540ada6ae664662c967cea
Josue A. <josueroyale8171@gmail.com>
2023-11-07 14:59:11 +0000

    Add nav

---
+++ app/components/path-nav.tsx
@@ -0,0 +1,17 @@
+import { ChevronRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+interface Props {
+  title: string;
+}
+
+export const PathNav = ({ title }: Props) => {
+  return (
+    <div className="w-full flex items-center gap-x-2">
+      <Link className="text-muted-foreground" href="/">Home</Link>
+      <ChevronRightIcon />
+      <Link className="text-muted-foreground" href="/">Posts</Link>
+      <ChevronRightIcon />
+      <Link className="font-semibold" href="/home">{title}</Link>
+    </div>
+  );
+};
--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -1,17 +1,23 @@
 import "katex/dist/katex.min.css";
 import { ReactNode } from "react";
+import glob from "fast-glob";
+import { Footer } from "../components/footer";
 
 interface Props {
   children: ReactNode;
+  params: any;
 }
+
+export const dynamic = "force-static";
 
 export default async function Layout({ children }: Props) {
 
   return (
     <main className="font-sans w-full h-full min-h-screen flex flex-col px-2 pb-20">
-      <article className="max-w-lg w-full px-2 mx-auto mt-10 prose">
-        {children}
+      <article className="max-w-lg w-full px-2 mx-auto mt-10">
+        <div className="w-full prose">{children}</div>
       </article>
+      <Footer />
     </main>
   );
 }
